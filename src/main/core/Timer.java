65e0291beabedaaa9a195a475c99a95dc8247a17
tigoilati2706 <83336888+tigoilati2706@users.noreply.github.com>
2022-05-14 13:06:41 +0700

    Update Timer.java

--- src/main/core/Timer.java
+++ src/main/core/Timer.java
@@ -1,6 +1,56 @@
-package main.core;
+import javax.swing.*;
+import java.awt.*;
 
-import java.util.Timer;
+public class Timer extends JLabel {
+    private boolean go;
+    private Runnable task;
 
-public class Clock {
+    public Timer() {
+        super("0", SwingConstants.CENTER);
+        setBorder(BorderFactory.createEmptyBorder(5, 0, 5, 0));
+        setFont(new Font(Font.MONOSPACED, Font.BOLD, 20));
+        task = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    // do nothing, this is fine
+                }
+                while (go) {
+                    setText(String.valueOf(Integer.valueOf(getText()) + 1));
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        // do nothing, this is fine
+                    }
+
+                }
+            }
+        };
+        go = false;
+    }
+
+    /**
+     * Starts the timer
+     */
+    public void start() {
+        go = true;
+        new Thread(task).start();
+    }
+
+    /**
+     * Stops the timer
+     */
+    public void stop() {
+        go = false;
+    }
+
+    /**
+     * Resets the timer
+     */
+    public void reset() {
+        go = false;
+        setText("0");
+    }
 }
